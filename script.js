ccba23bc6bce67a084dc31d9d64ebea60d3d4a5f
KyleGru <136273960+KyleGru@users.noreply.github.com>
2023-11-19 11:12:27 -0500

    added time blocks to the html, added functions to update time and capability to save to local storage in script file.

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+$(document).ready(function () {
+
+  function updateDateTime() {
+  var currentTime = dayjs()
+  $('#currentDay').html(currentTime.format('dddd, MMMM DD YYYY, h:mm:ss A'))
+
+  $('.time-block').each(function() {
+    var blockHour = dayjs().set('hour', parseInt($(this).attr('id').split('hour-')[1]))
+
+    if (blockHour.isBefore(currentTime, 'hour')) {
+      console.log('past')
+      $(this).addClass('past').removeClass('present future')
+    } else if (blockHour.isSame(currentTime, 'hour')) {
+      console.log('present')
+      $(this).addClass('present').removeClass('past future')
+    } else {
+      $(this).addClass('future').removeClass('past present')
+      console.log('future')
+    }
+  })
+}
+  
+updateDateTime()
+setInterval(updateDateTime, 1000)
+ 
+ 
+  
+  $('.saveBtn').on('click', function() {
+    var text = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+
+    localStorage.setItem(time, text);
+  })
+
+  for (var i = 9; i <= 17; i++) {
+    $('#hour-' + i + ' .description').val(localStorage.getItem('hour-' + i))
+  }
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +59,5 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+ 
 });

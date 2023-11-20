eb8d00731d708157f9d3c42050164bcb47b6bf39
KyleGru <136273960+KyleGru@users.noreply.github.com>
2023-11-20 18:18:51 -0500

    Commented on the HTML and JS files. Added content to the README.md file.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Work-Day-Scheduler
+
+## Description
+In this challenge I worked off of some starter code provided for me in HTML, CSS, and JavaScript. In the HTML file I added some more blocks of time to be able to schedule out normal 9-5 workday. In the JavaScipt file I added a function to be able to track the date and time using the Day.js link in the HTML. I then created a function to call to that time and block out what hours were available in the current work day. Hours that have passed will be blocked out in gray and the current hour will be highlighted in red. Any future hours are represented as green boxes. There is also the capability to save scheduled text to the local storage for future reference.
+
+I made this quiz with the following files
+-index.html
+-style.css
+-script.js
+
+## Visuals
+<img src="https://i.imgur.com/mle5O02.png" alt="Work-Day-Scheduler">
+<img src="https://i.imgur.com/dVimX0b.png" alt="script.js-code">
+
+## Resources
+https://github.com/comp7589/WorkDayScheduler/tree/master
+https://github.com/ThomasCalle/Online-Workday-Planner/blob/main/assets/script.js
+https://api.jquery.com/addClass/
+https://api.jquery.com/removeClass/
+https://api.jquery.com/ready/
+https://getbootstrap.com/docs/5.3/getting-started/introduction/
+## Links
+
+
+# Work-Day-Scheduler
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
+// This ensures that the function runs after the HTML and CSS is loaded.
 $(document).ready(function () {
 
+  // Grabs the date and time from dayjs and generates in the window.
   function updateDateTime() {
   var currentTime = dayjs()
   $('#currentDay').html(currentTime.format('dddd, MMMM DD YYYY, h:mm:ss A'))
 
+  // Operates through dayjs to update the specific times that have passed, what time it currently is, and what times are available in the future.
   $('.time-block').each(function() {
     var blockHour = dayjs().set('hour', parseInt($(this).attr('id').split('hour-')[1]))
 
@@ -28,7 +28,7 @@ updateDateTime()
 setInterval(updateDateTime, 1000)
  
  
-  
+// Click listener for saveBtn to save to the local storage.
   $('.saveBtn').on('click', function() {
     var text = $(this).siblings('.description').val();
     var time = $(this).parent().attr('id');
@@ -36,28 +36,10 @@ setInterval(updateDateTime, 1000)
     localStorage.setItem(time, text);
   })
 
+  // This for loop allows the text that is entered in the description areas to be saved and remain on the scheduler when the page is refreshed.
   for (var i = 9; i <= 17; i++) {
     $('#hour-' + i + ' .description').val(localStorage.getItem('hour-' + i))
   }
 
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
- 
 });
